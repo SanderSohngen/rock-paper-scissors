9ded3288b11d3fad1603fe6d6cb04a80380759c9
sander <sander.sohngen@usp.br>
2021-11-25 11:57:18 -0300

    implemented a simple UI

--- script.js
+++ script.js
@@ -1,62 +1,78 @@
-function computerPlay() {
-    const options = {
-        1: "rock",
-        2: "paper",
-        3: "scissors",
-    };
-    return options[Math.floor(Math.random() * 3) + 1];
+const score = document.createElement("div");
+const playerScore = document.createElement("p");
+const computerScore = document.createElement("p");
+score.appendChild(playerScore);
+score.appendChild(computerScore);
+document.body.appendChild(score);
+
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((key) => {
+	key.addEventListener("click", playRound);
+});
+
+function playRound() {
+	if (playerScoreCount === 5 || computerScoreCount === 5) return;
+	const playerChoice = this.textContent.toLowerCase();
+	const computerChoice = playComputer();
+	const winner = decideWinner(playerChoice, computerChoice);
+	updateScore(winner);
+	const message = writeMessage(winner, playerChoice, computerChoice);
+	appendResult(message);
+	if (playerScoreCount === 5 || computerScoreCount === 5) appendFinalResult(winner);
+}
+
+function playComputer() {
+	const options = ["rock", "paper", "scissors"];
+	return options[Math.floor(Math.random() * 3)];
 }
 
 function decideWinner(player, computer) {
-    const options = {
-        rock: 1,
-        paper: 2,
-        scissors: 3,
-    };
-    if (options[player] === options[computer]) return "draw";
-    if (options[player] - options[computer] === 1 || options[player] - options[computer] === -2) return "player";
-    return "computer";
+	const options = {
+		rock: 1,
+		paper: 2,
+		scissors: 3,
+	};
+	if (options[player] === options[computer]) return "draw";
+	if (
+		options[player] - options[computer] === 1 ||
+		options[player] - options[computer] === -2
+	)
+		return "player";
+	return "computer";
 }
 
-function playRound() {
-    const player = prompt("Choose rock, paper or scissors:").toLowerCase();
-    const computer = computerPlay();
-    return decideWinner(player, computer);
+function updateScore(winner) {
+	if (winner === "player") playerScoreCount++;
+	else if (winner === "computer") computerScoreCount++;
+	playerScore.textContent = `You've won ${playerScoreCount} rounds`;
+	computerScore.textContent = `Computer has won ${computerScoreCount} rounds`;
 }
 
-function writeMessage(winner) {
-    switch (winner) {
-        case "draw":
-            return "It's a draw";
-        case "player":
-            return "You won this round";
-        case "computer":
-            return "You lost this round"
-    }
+function writeMessage(winner, playerChoice, computerChoice) {
+	switch (winner) {
+		case "draw":
+			return `It's a draw, you both chose ${playerChoice}`;
+		case "player":
+			return `You won this round, ${playerChoice} beats ${computerChoice}`;
+		case "computer":
+			return `You lost this round, ${computerChoice} beats ${playerChoice}`;
+	}
 }
 
-function game() {
-    let playerCount = 0;
-    let computerCount = 0;
-    let drawCount = 0;
-    for (let i = 0; i < 5; i++) {
-        const winner = playRound();
-        switch (winner) {
-            case "draw":
-                drawCount++;
-                break;
-            case "player":
-                playerCount++;
-                break;
-            case "computer":
-                computerCount++;
-                break;
-        }
-        console.log(writeMessage(winner));
-    }
-    if (playerCount > computerCount) console.log("Congrats, you won the game! You won " + playerCount + " rounds.");
-    else if (computerCount > playerCount) console.log("You lost the game! You lost " + computerCount + " rounds.");
-    else console.log("It's a draw! You've both won " + playerCount + " rounds and drawn " + drawCount + " rounds.");
+function appendResult(message) {
+	const div = document.createElement("div");
+	div.textContent = message;
+	document.body.appendChild(div);
 }
 
-game();
+function appendFinalResult(winner) {
+	const final = document.createElement("div");
+	let msg = "";
+	if (winner === "player") msg = "You've won the game! Grats";
+	else msg = "You've lost the game! Get rekt";
+	final.textContent = msg;
+	document.body.appendChild(final);
+}
